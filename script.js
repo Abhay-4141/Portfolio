6b2e044e3dc3919bb51e682718ccbf1699a4702f
Abhay-4141 <patilabhay659@gmail.com>
2024-09-24 11:55:25 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Message sent successfully!");
+    // Here, you could also add form validation or send form data to a server.
+});
